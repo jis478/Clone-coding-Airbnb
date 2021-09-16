abdfbe74d624a91b91409f3f2ff37a75c71ba3fe
jis478 <marksk82@gmail.com>
2021-09-16 10:25:37 +0900

    user model added

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
-
-# Register your models here.
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin by Mark Kim"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
