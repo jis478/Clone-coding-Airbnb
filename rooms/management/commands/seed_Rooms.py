2575b3af7723be4c739efad5f43ba9987d883855
jis478 <marksk82@gmail.com>
2021-10-03 18:24:16 +0900

    photo added 2

--- rooms/management/commands/seed_Rooms.py
+++ rooms/management/commands/seed_Rooms.py
@@ -2,9 +2,9 @@ import random
 from django.core.management.base import BaseCommand
 from django.contrib.admin.utils import flatten
 from users.models import User as user_model
-from rooms.models import Photo, Room as room_model
+from rooms import models
+from rooms.models import Room as room_model
 from rooms.models import Photo as photo_model
-
 from rooms.models import RoomType as roomtype_model
 
 from django_seed import Seed
@@ -42,13 +42,29 @@ class Command(BaseCommand):
         created_rooms = seeder.execute()
         created_rooms_pk = flatten(list(created_rooms.values()))  # created rooms' pks
 
+        amenities = models.Amenity.objects.all()
+        house_rules = models.HouseRule.objects.all()
+        facilities = models.Facility.objects.all()
+
         for pk in created_rooms_pk:
-            room = room_model.objects.get(pk=pk)
-            for i in range(3, random.randint(5, 10)):
+            room = room_model.objects.get(pk=pk)  # foreign key for photo_model
+            for i in range(
+                3, random.randint(5, 10)
+            ):  # photo objects are generated and refered by "room" (foreign key)
                 photo_model.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"/room_photos/{random.randint(1,31)}.webp",
                 )
+
+        for a in amenities:
+            if float(random.random()) > 0.5:
+                room.amenities.add(a)
+        for h in house_rules:
+            if float(random.random()) > 0.5:
+                room.house_rules.add(h)
+        for f in facilities:
+            if float(random.random()) > 0.5:
+                room.facilities.add(f)
 
         self.stdout.write(self.style.SUCCESS(f"{number} room(s) created"))
