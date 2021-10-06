78f0bca0f11f3e5e0219911d2c80146bf140ff9b
jis478 <marksk82@gmail.com>
2021-10-06 21:24:05 +0900

    homeview added

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 
 def all_rooms(request):
-    pass
+    page = int(request.GET.get("page", 1)) # Query dictionary
+    limit = 10 * page
+    offset = limit - 10
+    rooms = models.Room.objects.all()[offset:limit]
+    return render(request, "rooms/home.html", context={"rooms": rooms})
