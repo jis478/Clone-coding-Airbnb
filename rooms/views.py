db190fb56c33c8ce34f59e5d3b6caf6dc9b991a9
jis478 <marksk82@gmail.com>
2021-10-14 10:07:05 +0900

    more search fields added

--- rooms/views.py
+++ rooms/views.py
@@ -33,10 +33,38 @@ class RoomDetail(DetailView):
     template_name = "rooms/room_detail.html"
 
 
+# values to be recevied from the form
+# values to be sent to the form
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    # countries = request.GET.get("countries")
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    price = int(request.GET.get("price", 0))
+    baths = int(request.GET.get("baths", 0))
+
+    options = {"countries": countries, "room_types": room_types, "amenities": amenities, "facilities":facilities}
+    forms = {
+        "selected_city": city,
+        "selected_country": country,
+        "selected_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+    }
+
     return render(
-        request, "rooms/search.html", context={"city": city, "countries": countries}
+        request,
+        "rooms/search.html",
+        context={**options, **forms}
+        # context={"city": city, "countries": countries, "room_types": room_types},
     )
