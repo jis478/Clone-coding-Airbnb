f4b636f920464a6e201a26a6d2498ae82e7a4600
jis478 <marksk82@gmail.com>
2021-10-17 13:42:32 +0900

    form added

---
+++ rooms/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.db.models.constraints import CheckConstraint
+from django.db.models.fields import CharField
+from django.forms.widgets import CheckboxSelectMultiple
+from django_countries.fields import CountryField
+from . import models
+
+
+class SearchForm(forms.Form):
+
+    city = forms.CharField(initial="Anywhere")
+    price = forms.IntegerField(initial=False)
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(
+        queryset=models.RoomType.objects.all(), required=False, empty_label="Any Kind"
+    )
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelChoiceField(queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple)
+    facilities = forms.ModelChoiceField(queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple)
--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django_countries import countries
-from . import models
+from . import models, forms
 
 
 class Homeview(ListView):
@@ -35,36 +35,110 @@ class RoomDetail(DetailView):
 
 # values to be recevied from the form
 # values to be sent to the form
+# def search(request):
+# city = request.GET.get("city", "Anywhere")
+# city = str.capitalize(city)
+# country = request.GET.get("country", "KR")
+# room_type = int(request.GET.get("room_type", 0))
+# room_types = models.RoomType.objects.all()
+# amenities = models.Amenity.objects.all()
+# facilities = models.Facility.objects.all()
+
+# guests = request.GET.get("guests", "0")  ## error!
+# # print("geee", guests)  # why return None?
+# # print(type(guests))
+# # print(guests)
+# bedrooms = int(request.GET.get("bedrooms", 0))
+# beds = int(request.GET.get("beds", 0))
+# price = int(request.GET.get("price", 0))
+# baths = int(request.GET.get("baths", "0"))
+# # print("baths", baths)
+
+# instant_book = bool(request.GET.get("instant_book", False))
+# superhost = bool(request.GET.get("superhost", False))
+
+# selected_amenities = request.GET.getlist("amenities")
+# selected_facilities = request.GET.getlist("facilities")
+
+# print(selected_amenities)
+
+# options = {
+#     "countries": countries,
+#     "room_types": room_types,
+#     "amenities": amenities,
+#     "facilities": facilities,
+# }
+# forms = {
+#     "selected_city": city,
+#     "selected_country": country,
+#     "selected_room_type": room_type,
+#     "price": price,
+#     "guests": guests,
+#     "bedrooms": bedrooms,
+#     "beds": beds,
+#     "baths": baths,
+#     "instant_book": instant_book,
+#     "superhost": superhost,
+#     "selected_amenities": selected_amenities,
+#     "selected_facilities": selected_facilities,
+# }
+
+# filter_args = {}
+
+# if city != "Anywhere":  # exclude default
+#     filter_args["city__startswith"] = city
+
+# filter_args["country"] = country
+
+# if room_type != 0:
+#     filter_args["room_type__pk__exact"] = room_type  # room_type is a foreign key
+
+# if price != 0:
+#     filter_args["price__lte"] = price
+
+# if guests != 0:
+#     filter_args["price__gte"] = guests
+
+# if bedrooms != 0:
+#     filter_args["bedrooms__gte"] = bedrooms
+
+# if beds != 0:
+#     filter_args["beds__gte"] = beds
+
+# if baths != 0:
+#     filter_args["baths__gte"] = baths
+
+# if instant_book is True:
+#     filter_args["instant_book"] = True
+
+# if superhost is True:
+#     filter_args["host__superhost"] = True
+
+# if len(selected_amenities) > 0:
+#     for a in selected_amenities:
+#         filter_args["amenities__pk"] = int(a)
+
+# if len(selected_facilities) > 0:
+#     for f in selected_facilities:
+#         filter_args["facilities__pk"] = int(f)
+
+# print(filter_args)
+
+# rooms = models.Room.objects.filter(**filter_args)
+
+# return render(
+#     request,
+#     "rooms/search.html",
+#     context={**options, **forms, "rooms": rooms}
+#     # context={"city": city, "countries": countries, "room_types": room_types},
+# )
+
+
 def search(request):
-    city = request.GET.get("city", "Anywhere")
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
-
-    guests = int(request.GET.get("guests", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    beds = int(request.GET.get("beds", 0))
-    price = int(request.GET.get("price", 0))
-    baths = int(request.GET.get("baths", 0))
-
-    options = {"countries": countries, "room_types": room_types, "amenities": amenities, "facilities":facilities}
-    forms = {
-        "selected_city": city,
-        "selected_country": country,
-        "selected_room_type": room_type,
-        "price": price,
-        "guests": guests,
-        "bedrooms": bedrooms,
-        "beds": beds,
-        "baths": baths,
-    }
-
+    form = forms.SearchForm(request.GET)
     return render(
         request,
         "rooms/search.html",
-        context={**options, **forms}
+        context={"form": form}
         # context={"city": city, "countries": countries, "room_types": room_types},
     )
