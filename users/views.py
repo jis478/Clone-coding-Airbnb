633252e22e3ace8869f8e6f7236b573080276740
jis478 <marksk82@gmail.com>
2021-11-07 22:05:18 +0900

    github login details added

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
@@ -74,7 +75,7 @@ def complete_verification(request, key):
 
 
 def github_login(request):
-    client_id = os.enviorn.get("GH_ID")
+    client_id = os.environ.get("GH_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
     return redirect(
         f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
@@ -82,4 +83,14 @@ def github_login(request):
 
 
 def github_callback(request):
-    pass
+    client_id = os.environ.get("GH_ID")
+    client_secret = os.environ.get("GH_SECRET")
+    code = request.GET.get("code", None)
+    if code is not None:
+        request = requests.post(
+            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+            headers={"Accept": "application/json"},
+        )
+        print(request.json())
+    else:
+        return redirect(reverse("core:home"))
