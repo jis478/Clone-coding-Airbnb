177588804f39c87168c4a4c1462be0d8872a495d
jis478 <marksk82@gmail.com>
2021-11-17 21:48:38 +0900

    kakao login completed

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from django.forms.widgets import PasswordInput
 from . import models
 
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -16,27 +17,43 @@ class LoginForm(forms.Form):
             if user.check_password(password):
                 return self.cleaned_data
             else:
-                raise self.add_error(
-                    "password", forms.ValidationError("Wrong password")
-                )
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            raise self.add_error("email", forms.ValidationError("Email doesn't exist"))
+            self.add_error("email", forms.ValidationError("User does not exist"))
 
 
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
-        fields = {"first_name", "last_name", "email"}
+        fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+        }
 
-    password = forms.CharField(widget=PasswordInput)
-    password1 = forms.CharField(widget=PasswordInput, label="Password Repeat")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")
-
         if password != password1:
-            raise forms.ValidationError("Password confirmation error")
+            raise forms.ValidationError("Password confirmation does not match")
         else:
             return password
 
@@ -45,40 +62,5 @@ class SignUpForm(forms.ModelForm):
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
         user.username = email
-        user.set_password(password)  # password encryption
+        user.set_password(password)
         user.save()
-
-
-# class SignUpForm(forms.Form):
-#     first_name = forms.CharField(max_length=80)
-#     last_name = forms.CharField(max_length=80)
-#     email = forms.EmailField()
-#     password = forms.CharField(widget=PasswordInput)
-#     password1 = forms.CharField(widget=PasswordInput, label="Password Repeat")
-
-#     def clean_email(self):
-#         email = self.cleaned_data.get("email")
-#         try:
-#             models.User.objects.get(email=email)
-#             raise forms.ValidationError("User already exists")
-#         except models.User.DoesNotExist:
-#             return email
-
-#     def clean_password1(self):
-#         password = self.cleaned_data.get("password")
-#         password1 = self.cleaned_data.get("password1")
-
-#         if password != password1:
-#             raise forms.ValidationError("Password confirmation error")
-#         else:
-#             return password
-
-#     def save(self):
-#         first_name = self.cleaned_data("first_name")
-#         last_name = self.cleaned_data("last_name")
-#         email = self.cleaned_data("email")
-#         password = self.cleaned_data("password")
-#         user = models.User.objects.create_user(email, email, password)
-#         user.first_name = first_name
-#         user.last_name = last_name
-#         user.save()
